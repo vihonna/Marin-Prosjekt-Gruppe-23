1247c95a8b08d2d92a1e3d5a1362d696b273747e
vihonna <75090272+vihonna@users.noreply.github.com>
2021-10-20 15:02:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
-# coding=utf-8
-# This is a sample Python script.
+import numpy
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-# les_input er en funksjon som tar inn data fra en fil og skriver ut formatet
 
 def get_knutepunkt(filename):
     array = les_input(filename)
-    return(array[0])
+    return array[0]
+
 
 def get_elementer(filename):
     array = les_input(filename)
-    return(array[1])
+    return array[1]
+
 
 def get_laster(filename):
     array = les_input(filename)
-    return(array[2])
+    return array[2]
+
+
+def get_Iprofil(int):
+    return 1000000
+
+def get_areal(param):
+    return 10000
+
 
 def les_input(filename):
     info = []
@@ -29,7 +34,7 @@ def les_input(filename):
         laster     = []
         total_array= []
         n_element = 1
-        for i in range(1, len(info)):
+        for i in range(0, len(info)):
             arr = list(map(int, info[i].split()))
             if i < n_punkt:
                 knutepunkt.append(arr)
@@ -38,22 +43,48 @@ def les_input(filename):
                 n_element = n_element + 1
             if i > (n_punkt + n_element):
                 laster.append(arr)
-        print(laster)
         total_array.append(knutepunkt)
         total_array.append(elementer)
         total_array.append(laster)
         return(total_array)
 
-    # Use a breakpoint in the code line below to debug your script.
 
+def local_k(filename):
+
+    elementer = get_elementer('rammedata.txt')
+    knutepunkt = get_knutepunkt('rammedata.txt')
+    laster = get_laster('rammedata.txt')
+
+    for i in range(0, len(elementer)):
+        ende1 = elementer[i][0]
+        ende2 = elementer[i][1]
+
+        x_ende1 = knutepunkt[ende1][0]
+        y_ende1 = knutepunkt[ende1][1]
+        x_ende2 = knutepunkt[ende2][0]
+        y_ende2 = knutepunkt[ende2][1]
+        L = numpy.sqrt((x_ende1 - x_ende2)**2 + (y_ende1 - y_ende2)**2)
+        E = elementer[i][2]
+        I = get_Iprofil(elementer[i][3])
+        A = get_areal(elementer[i][3])
+
+        k = numpy.array([[E * A / L, 0., 0., E * A / L, 0., 0.],
+                         [0., 12 * E * I / (L ** 3), -6 * E * I / (L ** 2), 0., -12 * E * I / (L ** 3),
+                          -6 * E * I / (L ** 2)],
+                         [0., -6 * E * I / (L ** 2), 4 * E * I / L, 0., 6 * E * I / (L ** 2), 2 * E * I / L],
+                         [E * A / L, 0., 0., E * A / L, 0., 0.],
+                         [0., -12 * E * I / (L ** 3), 6 * E * I / (L ** 2), 0., 12 * E * I / (L ** 3), 6 * E * I / L],
+                         [0., -6 * E * I / (L ** 2), 2 * E * I / L, 0., 6 * E * I / L, 4 * E * I / L]])
+
+
+
+
+
+    #Use a breakpoint in the code line below to debug your script.
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     get_knutepunkt('rammedata.txt')
     get_elementer('rammedata.txt')
     get_laster('rammedata.txt')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
-#testing2
+    local_k('rammedata.txt')
