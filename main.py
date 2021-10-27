58aef8e68b771519b3af1e590c3f583072910602
vihonna <75090272+vihonna@users.noreply.github.com>
2021-10-27 11:06:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,22 +17,24 @@ def get_laster(filename):
 
 
 def get_Iprofil(int):
-    return 1000000
+    return 1
+
 
 def get_areal(param):
-    return 10000
+    return 0.1
 
 
 def les_input(filename):
     info = []
     with open(filename) as f:
+        global n_punkt
         n_punkt = int(f.readline())
         info = f.readlines()
-
+        global n_element
         knutepunkt = []
-        elementer  = []
-        laster     = []
-        total_array= []
+        elementer = []
+        laster = []
+        total_array = []
         n_element = 1
         for i in range(0, len(info)):
             arr = list(map(int, info[i].split()))
@@ -46,11 +48,42 @@ def les_input(filename):
         total_array.append(knutepunkt)
         total_array.append(elementer)
         total_array.append(laster)
-        return(total_array)
+        return total_array
 
 
-def local_k(filename):
+def sysMatrise_setup():
+    global SysMatrise
+    SysMatrise = numpy.zeros((3*n_punkt, 3*n_punkt))
 
+
+def sysMatrise_adder(ki, number, ende1, ende2):
+    if number == 0:
+        for i in range(3):
+            for j in range(3):
+                SysMatrise[ende1 + i][ende1 + j] = ki[i][j]
+    if number == 1:
+        for i in range(3):
+            for j in range(3):
+                SysMatrise[ende2 + i][ende2 + j] = ki[i][j]
+    if number == 2:
+        for i in range(3):
+            for j in range(3):
+                SysMatrise[ende2 + i][ende1 + j] = ki[i][j]
+    if number == 3:
+        for i in range(3):
+            for j in range(3):
+                SysMatrise[ende2 + i][ende2 + j] = ki[i][j]
+
+
+def get_Tmatrise(theta):
+    s = numpy.sin(theta)
+    c = numpy.cos(theta)
+    T_matrise = numpy.array([[c, -s, 0, 0, 0, 0], [s, c, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0],
+                             [0, 0, 0, c, -s, 0], [0, 0, 0, s, c, 0], [0, 0, 0, 0, 0, 1]])
+    return T_matrise
+
+
+def kMatrise(filename):
     elementer = get_elementer('rammedata.txt')
     knutepunkt = get_knutepunkt('rammedata.txt')
     laster = get_laster('rammedata.txt')
@@ -63,12 +96,12 @@ def local_k(filename):
         y_ende1 = knutepunkt[ende1][1]
         x_ende2 = knutepunkt[ende2][0]
         y_ende2 = knutepunkt[ende2][1]
-        L = numpy.sqrt((x_ende1 - x_ende2)**2 + (y_ende1 - y_ende2)**2)
+        L = numpy.sqrt((x_ende1 - x_ende2) ** 2 + (y_ende1 - y_ende2) ** 2)
         E = elementer[i][2]
         I = get_Iprofil(elementer[i][3])
         A = get_areal(elementer[i][3])
-
-        k = numpy.array([[E * A / L, 0., 0., E * A / L, 0., 0.],
+        theta = numpy.arcsin((y_ende2 - y_ende1) / L)
+        k = numpy.array([[E * A / L, 0., 0., -E * A / L, 0., 0.],
                          [0., 12 * E * I / (L ** 3), -6 * E * I / (L ** 2), 0., -12 * E * I / (L ** 3),
                           -6 * E * I / (L ** 2)],
                          [0., -6 * E * I / (L ** 2), 4 * E * I / L, 0., 6 * E * I / (L ** 2), 2 * E * I / L],
@@ -76,15 +109,21 @@ def local_k(filename):
                          [0., -12 * E * I / (L ** 3), 6 * E * I / (L ** 2), 0., 12 * E * I / (L ** 3), 6 * E * I / L],
                          [0., -6 * E * I / (L ** 2), 2 * E * I / L, 0., 6 * E * I / L, 4 * E * I / L]])
 
+        T = get_Tmatrise(theta)
+        kg_temp = numpy.dot(T, k)
+        T_inv = numpy.linalg.inv(T)
+        kg = numpy.dot(kg_temp, T_inv)
 
+        k1 = [[kg[0][0], kg[0][1], kg[0][2]], [kg[1][0], kg[1][1], kg[1][2]], [kg[2][0], kg[2][1], kg[2][2]]]
+        k2 = [[kg[0][3], kg[0][4], kg[0][5]], [kg[1][3], kg[1][4], kg[1][5]], [kg[2][3], kg[2][4], kg[2][5]]]
+        k3 = [[kg[3][0], kg[3][1], kg[3][2]], [kg[4][0], kg[4][1], kg[4][2]], [kg[5][0], kg[5][1], kg[5][2]]]
+        k4 = [[kg[3][3], kg[3][4], kg[3][5]], [kg[4][3], kg[4][4], kg[4][5]], [kg[5][3], kg[5][4], kg[5][5]]]
+        k_mini = numpy.array([k1, k2, k3, k4])
+        for l in range(4):
+            sysMatrise_adder(k_mini[l], l, ende1, ende2)
 
-
-
-    #Use a breakpoint in the code line below to debug your script.
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    get_knutepunkt('rammedata.txt')
-    get_elementer('rammedata.txt')
-    get_laster('rammedata.txt')
-    local_k('rammedata.txt')
+    les_input('rammedata.txt')
+    sysMatrise_setup()
+    kMatrise('rammedata.txt')
+    print(SysMatrise)
